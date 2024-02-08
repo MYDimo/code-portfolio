0578def283f505ed026bfcd500c3d5b2f87d05db
Mihaylo Dimo <mihaylo.dimo@gmail.com>
2024-02-08 16:57:05 +0200

    Change Netlify adapter with Vercel one

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,16 @@ import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import mdx from "@astrojs/mdx";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  adapter: netlify({
-    imageCDN: false,
-  }),
-  integrations: [tailwind(), react(), mdx()],
-  image: {
-    domains: ["astro.build"],
-  }
+  adapter: vercel(),
+  integrations: [tailwind(), react(), mdx()]
+  // image: {
+  //   domains: ["astro.build"],
+  // }
 });
